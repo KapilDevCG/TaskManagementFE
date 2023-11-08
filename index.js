3e7bb94e64f49e7dadc0fe2cc037a9252f3f2af3
KapilDevCG <kapil.b.dev@capgemini.com>
2023-11-08 15:03:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app=express();
+const port=4000;
+const API_URL="http://localhost:3000";
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//initial content
+app.get("/", async (req, res)=>{
+    try{
+    const result = await axios.get(API_URL);
+    res.render("index.ejs", {
+        content: result.data
+    });
+    } catch (error){
+        res.render("index.ejs", {
+            content: error
+        });
+    }
+});
+
+//Get by id
+app.get("/api/posts/complete/:id", async (req,res)=>{
+    const newBody={completed: true};
+    try{
+    const result= await axios.patch(API_URL+"/tasks/"+req.params.id, newBody);
+    res.redirect("/");
+    }catch(error){
+        res.render("index.ejs", {
+            content: error
+        });
+    }
+
+});
+
+//Add Page
+app.get("/new", (req, res) => {
+    res.render("modify.ejs", { heading: "New Task", submit: "Create Task" });
+  });
+
+  //Delete
+app.get("/api/posts/delete/:id", async (req, res)=>{
+    try{
+    const result= await axios.delete(API_URL+"/tasks/"+req.params.id);
+    console.log(result.data);
+    res.redirect("/");
+    }catch(error){
+        res.status(500).json({error: error});
+    }
+});
+
+//update using patch
+app.get("/edit/:id", async (req, res)=>{
+
+    try{
+    const result= await axios.get(API_URL+"/tasks/"+req.params.id);
+
+    res.render("modify.ejs", {
+        heading: "Update Task",
+        submit: "Update Task",
+        content: result.data
+    });
+
+    console.log(result.data);
+    }catch(error){
+        res.status(500).json({error: error});
+    }
+
+});
+
+app.post("/api/posts/:id", async (req, res)=>{
+    try{
+    const result= await axios.patch(API_URL+"/tasks/"+req.params.id, req.body);
+    console.log(result.data);
+    res.redirect("/");
+    }catch(error){
+        res.status(500).json({error: error});
+    }
+
+});
+
+//add to db
+app.post("/api/posts", async (req, res)=>{
+    try{
+    const result= await axios.post(API_URL+"/tasks", req.body);
+    console.log(result.data);
+    res.redirect("/");
+    }catch(error){
+        res.status(500).json({error: error});
+    }
+    
+});
+
+app.listen(port, ()=>{
+    console.log(`Client started at port: ${port}`);
+})
